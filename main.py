b17b473a1253b0cd9d515b0c930746f5b7285d42
Hemanth B <hemanth22hemu@gmail.com>
2024-07-30 00:12:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+import ansible_runner
 import os
-print("Current Directory:", os.getcwd())
+
+def run_playbook(playbook_path):
+    print("Current Directory:", os.getcwd())
+    
+    if not os.path.isfile(playbook_path):
+        print(f"Error: The playbook file {playbook_path} does not exist.")
+        return
+    
+    result = ansible_runner.run(playbook=playbook_path)
+    
+    if result.status == 'successful':
+        print("Playbook executed successfully.")
+    else:
+        print("Playbook execution failed.")
+    
+    print("Status:", result.status)
+    print("Stats:", result.stats)
+    print("RC:", result.rc)
+    print("Events:", list(result.events))
+
+if __name__ == "__main__":
+    playbook_path = 'helloworld.playbook'
+    run_playbook(playbook_path)
